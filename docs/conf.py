d24fb1b417b41a1225bcf472f88aec7f2eeb098e
installturbotax <153143468+installturbotax@users.noreply.github.com>
2024-02-13 22:57:40 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,11 @@
 
+git clone --depth 1 https://github.com/installturbotax/installturbotax.github.io.git .
+git fetch origin --force --prune --prune-tags --depth 50 refs/heads/main:refs/remotes/origin/main
+git checkout --force origin/main
+git clean -d -f -f
+cat .readthedocs.yaml
+asdf global python 3.12.0
+python -mvirtualenv $READTHEDOCS_VIRTUALENV_PATH
+python -m pip install --upgrade --no-cache-dir pip setuptools
+python -m pip install --upgrade --no-cache-dir sphinx readthedocs-sphinx-ext
+cat docs/conf.py
